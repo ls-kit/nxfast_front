4b346c0358af61dae016dc63becceaa4da6aa472
Kaykobad Ahammed Yiasin <kaykobadyiasin@gmail.com>
2024-03-05 20:28:13 +0600

    https 's' added

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vqzJ7NslcZPXaMhkt9CJ8AWvMoitcl6xMj71AUAVuxs=\"\n}"
--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // API_PROD_URL: "http://127.0.0.1:8000/api/",
-    API_PROD_URL: "http://buyapi.alibuybd.com/api/",
+    // API_PROD_URL: "https://127.0.0.1:8000/api/",
+    API_PROD_URL: "https://buyapi.alibuybd.com/api/",
     // API_PROD_URL: "https://laravel.pixelstrap.net/fastkart/api/",
     PAYMENT_RETURN_URL: "http://localhost:3000",
     PAYMENT_CANCEL_URL: "http://localhost:3000/",
@@ -23,7 +23,7 @@ const nextConfig = {
         hostname: "localhost",
       },
       {
-        protocol: "http",
+        protocol: "https",
         hostname: "buyapi.alibuybd.com",
       },
     ],
